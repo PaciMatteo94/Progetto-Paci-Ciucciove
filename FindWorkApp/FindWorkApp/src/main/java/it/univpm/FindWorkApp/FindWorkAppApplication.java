6076fe90d4187359052498754ce640b43c614a30
Paci Matteo <S1066475@studenti.univpm.it>
2021-08-20 18:49:26 +0200

    Start

---
+++ FindWorkApp/FindWorkApp/src/main/java/it/univpm/FindWorkApp/FindWorkAppApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.FindWorkApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindWorkAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindWorkAppApplication.class, args);
+	}
+
+}
---
+++ FindWorkApp/FindWorkApp/src/test/java/it/univpm/FindWorkApp/FindWorkAppApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univpm.FindWorkApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindWorkAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
