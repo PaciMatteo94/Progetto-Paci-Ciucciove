a403cf3a8111cbadea8516c3f84bbd1464ea7643
Paci Matteo <S1066475@studenti.univpm.it>
2021-09-02 19:10:46 +0200

    Non aveva salvato il manager

--- FindWorkApp/FindWorkApp/src/main/java/it/univpm/FindWorkApp/Manager/Manager.java
+++ FindWorkApp/FindWorkApp/src/main/java/it/univpm/FindWorkApp/Manager/Manager.java
@@ -7,20 +7,21 @@ import org.json.simple.JSONObject;
 import it.univpm.FindWorkApp.APICall.APICall;
 import it.univpm.FindWorkApp.model.City;
 
-public class Manager implements ManagerService{
-	private APICall call= APICall.getInstance();
+public class Manager implements ManagerService {
+	private APICall call = APICall.getInstance();
 	private String[] location;
 	private ArrayList<City> cities;
-	private static Manager instance =null;
-	private Manager() {}
+	private static Manager instance = null;
+
+	private Manager() {
+	}
+
 	public static Manager getInstance() {
-		if(instance ==null) {
+		if (instance == null) {
 			instance = new Manager();
 		}
 		return instance;
 	}
-
-
 
 	@Override
 	public JSONObject getCities(String[] location, String employment_type) {
@@ -91,32 +92,35 @@ public class Manager implements ManagerService{
 
 		}
 	}
+
 	@Override
 	public JSONObject getStats() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public JSONObject getStats(String[] location) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public JSONObject getStats(String date) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
 	public JSONObject getStats(String[] location, String date) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
 	@Override
-	public JSONObject getStats(String[] location,  String date,boolean remote) {
+	public JSONObject getStats(String[] location, String date, boolean remote) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	
 
 }
